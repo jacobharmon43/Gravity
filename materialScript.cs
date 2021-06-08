66a91e2561f72a82501079373c7754de69870ec3
jacobharmon43 <64942817+jacobharmon43@users.noreply.github.com>
2021-06-08 05:28:45 -0500

    Update materialScript.cs

--- materialScript.cs
+++ materialScript.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class materialScript : MonoBehaviour
 {
-    public bool shouldMove = true;
+    private bool shouldMove = true;
     private GameObject player;
     private playerGunScript playerScript;
     private float gravityScale = 0.0f;
     private readonly float gravity = -9.81f;
     private Rigidbody rb;
-    // Start is called before the first frame update
+
+    // Variable Init
     void Start()
     {
         player = GameObject.Find("PlayerModel");
@@ -24,16 +25,27 @@ public class materialScript : MonoBehaviour
         }
     }
 
-    public void ChangeGravityScale(float change)
+    //This is turned off when an object is being traveled to the player
+    //May not be necessary if I move the movement to here.
+    public void SetShouldMove(bool set)
     {
-        gravityScale = change;
+        this.shouldMove = set;
     }
 
+    //Sets an objects gravity scale, used in physics
+    public void ChangeGravityScale(float change)
+    {
+        this.gravityScale = change;
+    }
+
+    //Just for other functions, obviously part of the get/set functions
+    //Purpose is just so I can set the gravity scale to what it was after grabbing
     public float ReadGravityScale()
     {
         return gravityScale;
     }
 
+    //Adds a gravity force multiplied by the scale, only runs if it shouldMove
     private void MoveByGrav()
     {
         rb = GetComponent<Rigidbody>();
@@ -41,6 +53,7 @@ public class materialScript : MonoBehaviour
         rb.AddForce(new Vector3(0, gravityScale * gravity * Time.deltaTime, 0), ForceMode.VelocityChange);
     }
 
+    //Drops itself if it hits a wall, this is a method to avoid clipping
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.CompareTag("Ground") || collision.transform.CompareTag("Interactible"))
